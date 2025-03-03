2f52d491ba10865fd9ff2e6b47e046d384f8fde8
stdthoth <ichthoth@gmail.com>
2025-03-03 16:15:57 +0100

    logo

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -12,7 +12,7 @@ const Header = () => {
         <div className="wrapper flex-between">
             <div className="flex-start">
                 <Link href="/" className='flex-start'>
-                    <Image src={'/images/logo.svg'} alt={`${APP_NAME} logo`}height={48} width={48}/>
+                    <Image src={'/images/ng-logo.png'} alt={`${APP_NAME} logo`}height={48} width={48}/>
                     <span className="hidden lg:block font-bold text-2xl ml-3">
                         {APP_NAME}
                     </span>
