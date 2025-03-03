b7ed4643a97cc7e0d9019098601777fe3cef9a9e
stdthoth <ichthoth@gmail.com>
2025-03-03 16:51:25 +0100

    sheet menu on smaller devices

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,7 @@
-import {ShoppingCart, UserIcon} from 'lucide-react'
 import Link from 'next/link'
 import Image from 'next/image';
-import { Button } from '@/components/ui/button';
 import { APP_NAME } from '@/lib/constants';
-import Toggle from './toggle';
+import Menu from './menu';
 
 
 const Header = () => {
@@ -19,17 +17,7 @@ const Header = () => {
                 </Link>
             </div>
             <div className="space-x-2">
-                <Toggle/>
-                <Button asChild variant='ghost'>
-                    <Link href='/cart'>
-                        <ShoppingCart/> Cart
-                    </Link>
-                </Button>
-                <Button asChild variant='ghost'>
-                    <Link href='/sign-in'>
-                        <UserIcon/> Sign In
-                    </Link>
-                </Button>
+                <Menu/>
             </div>
         </div>
     </header>
