4b849a117e763534c32021dc6c92b7ab09d765a8
stdthoth <ichthoth@gmail.com>
2025-03-03 15:34:28 +0100

    header and footer, dark mode toggle,

---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME||"Not Average";
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION||"Not Average id the number one streetwear fashion brand in Africa"
+export const SERVER_URL = process.env.NEXT_SERVER_URL||"http://localhost:3000"
