856c34bdc3feb9d184293b958c440b905e038afa
stdthoth <ichthoth@gmail.com>
2025-03-04 13:55:51 +0100

    prisma db migration and seeding

---
+++ db/sample-data.ts
@@ -0,0 +1,76 @@
+const sampleData = {
+  products: [
+    {
+      name: 'Notaverge New Religion Skull Hat',
+      slug: 'notavg-NR-skull-hat',
+      category: "Headwear",
+      description: 'Classic skull hat designed for comfort and style',
+      images: [
+        '/images/sample-products/not-avg-skull-cap.png'
+      ],
+      price: 20000.00,
+      brand: 'New Religion',
+      stock: 25,
+      isFeatured: true,
+      banner: 'banner-1.jpg',
+    },
+    {
+      name: 'Notaverage Black Joggers',
+      slug: 'notavg-black-joggers',
+      category: "Bottoms",
+      description: 'Joggers designed for timeless style and premium comfort',
+      images: [
+        '/images/sample-products/not-avg-black-joggers.png'
+      ],
+      price: 60000.00,
+      brand: 'Not Average',
+      stock: 10,
+      isFeatured: true,
+      banner: 'banner-2.jpg',
+    },
+    {
+      name: 'Not Average Grey Joggers',
+      slug: 'notavg-grey-joggers',
+      category: "Bottoms",
+      description: 'Joggers designed for timeless style and premium comfort',
+      images: [
+        '/images/sample-products/not-avg-grey-joggers.png'
+      ],
+      price: 60000.00,
+      brand: 'Not Average',
+      stock: 7,
+      isFeatured: false,
+      banner: null,
+    },
+    {
+      name: 'Not Average New Religion Shirt',
+      slug: 'not-avg-NR-shirt',
+      category: "Shirts",
+      description: 'Classic shirt designed for perfect fit and style',
+      images: [
+        '/images/sample-products/not-avg-NR-shirt.png'
+      ],
+      price: 50000.00,
+      brand: 'New Religion',
+      stock: 18,
+      isFeatured: false,
+      banner: null,
+    },
+    {
+      name: 'Not Average New Religion Tank Top',
+      slug: 'not-avg-NR-sleeveless-tank-top',
+      category: "Tank Tops",
+      description: 'Perfect fit Tank tops designed to show off your gym gains ;)',
+      images: [
+        '/images/sample-products/not-avg-NR-sleeveless-tank-top.png'
+      ],
+      price: 45000.00,
+      brand: 'New Religion',
+      stock: 18,
+      isFeatured: false,
+      banner: null,
+    },
+  ],
+};
+
+export default sampleData;
---
+++ db/seeder.ts
@@ -0,0 +1,15 @@
+import {Prisma, PrismaClient} from '@prisma/client';
+import sampleData from './sample-data';
+
+
+async function seed() {
+    const prisma = new PrismaClient();
+
+    await prisma.product.deleteMany();
+    await prisma.product.createMany({data: sampleData.products});
+
+    console.log('Seeding complete');
+}
+
+seed();
+
