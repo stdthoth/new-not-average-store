1fa81ad7292c6538c31449dbf44793c6169ec24e
stdthoth <ichthoth@gmail.com>
2025-03-04 21:13:59 +0100

    prisma

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,16 +1,22 @@
 'use server'
-import { PrismaClient } from "@prisma/client";
+import {prisma} from '@/db/prisma'
 import { convertToJSObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 
 
 export async function getProducts() {
-    const prisma = new PrismaClient();
-
     const data = await prisma.product.findMany({
         take: LATEST_PRODUCTS_LIMIT,
         orderBy: {createdAt:'desc'}
     });
 
     return convertToJSObject(data);
+}
+
+export async function getProductBySlug(slug: string) {
+    return await prisma.product.findFirst({
+        where: {
+            slug: slug
+        },
+    })
 }
