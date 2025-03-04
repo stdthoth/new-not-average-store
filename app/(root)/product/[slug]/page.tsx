c464e256609e87e19747c504100c27e81c2bf3d2
stdthoth <ichthoth@gmail.com>
2025-03-05 00:22:58 +0100

    fix page.tsx

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -25,19 +25,17 @@ const ProductDetailsPage = async (props:{
                 {/*Product Details Column*/}
                 <div className="col-span-2 p-5">
                     <div className="flex flex-col gap-6">
-                        <p>
-                            {product.brand} {product.category}
-                            <h1 className="h3-bold">{product.name}</h1>
-                            <div className="flex flex-col sm:flex-row sm:items-center gap-3">
-                                <ProductPrice value={Number(product.price)} className="w-24 rounded-full bg-green-100 text-green-700 px-5 py-2"/>
-                            </div>
-                        </p>
+                        <p>{product.brand} {product.category}</p>
+                        <h1 className="h3-bold">{product.name}</h1>
+                        <div className="flex flex-col sm:flex-row sm:items-center gap-3">
+                            <ProductPrice value={Number(product.price)} className="w-25 rounded-full bg-green-100 text-green-700 px-5 py-2"/>
+                        </div>
                     </div>
                     <div className="mt-10">
-                        <p className="font-semibold"></p>
+                        <p className="font-semibold">Description</p>
                         <p>{product.description}</p>
                     </div>
-                </div>
+                </div> 
                 {/*Add to Cart aColumn*/}
                 <div>
                     <Card>
